72fd3c4871a77fb59a9b45e38d227ebc0f3bd949
Kenneth Ezugworie <kennyblaq069@gmail.com>
2026-03-02 15:32:50 +0100

    committ

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function CLoseboard(){
  const mobileNav = document.getElementById("mobileNav");
     mobileNav.style.display = "none"
 }
+// const mobileNav = document.getElementById("mobileNav");
+// function OpenBar{
+// if (onClick){
+//     mobileNav.style.visibility ="visible"
+// }
+// else{
+// mobileNav.style.visibility ="hidden"
+// }
+// }
